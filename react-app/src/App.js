40ef3d06c233c5b001f672873bec88f97ede78a3
Ariunbold <ariun1118@gmail.com>
2020-07-15 18:29:15 -0400

    removed unnecessary files

---
+++ react-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'
+function App() {
+  return (
+    <div className="app">
+      <div className = 'app-header'>
+        <img src = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQPX3Ej-uhPxVXSoJZKSJ98Th53RSsKbit3ew&usqp=CAU" alt ="logo"  className = 'logo'/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
